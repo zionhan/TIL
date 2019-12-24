87642d30cb95efec66c7468e7a92ced0a93e6d95
zionhan <zionhan0708@gmail.com>
2019-12-24 22:38:57 +0900

    selection_sort using python

--- Algorithm/sort/selection_sort.py
+++ Algorithm/sort/selection_sort.py
@@ -1,10 +1,26 @@
 # 정렬 알고리즘 만큼 시간 복잡도의 효율성의 차이를 극명하게 보여주는 것이 없다.
-
-data = [ 1, 10, 5, 8, 7, 6, 4, 3, 2, 9 ]
+# 시간복잡도 O(N^2)
+import random
 
 def selection_sort( data ): 
     size = len(data)
-    print( size )
     
+    for i in range( 0, size-1 ) :
+        min = i
+        for j in range( i+1, size ) :
+            if( data[min] > data[j] ) :
+                min = j
+        
+        temp = data[i]
+        data[i] = data[min]
+        data[min] = temp
+    
+    return  
+        
+    
+    
+    
+data = random.sample( range(100), 10 )      
 selection_sort(data)
+print( data )
     
