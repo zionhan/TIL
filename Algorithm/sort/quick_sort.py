2a8278ad58c82b15e9c5bd708ec812d2dcb02329
zionhan <zionhan0708@gmail.com>
2019-12-30 19:41:00 +0900

    quick sort

--- Algorithm/sort/quick_sort.py
+++ Algorithm/sort/quick_sort.py
@@ -1,4 +1,11 @@
 # 시간복잡도  평균 O( NlogN ) 최악의 경우 O( N^2 ) 
+'''
+퀵정렬(Quick sort)
+ - 정렬 알고리즘의 꽃
+ - 기준점(pivot)을 정해서, 기준점보다 작은 데이터는 왼쪽(left), 큰 데이터는 오른쪽(right)로 모으는 함수를 작성함
+ - 각 왼쪽, 오른쪽은 재귀용법을 사용해서 다시 동일 함수를 호출하여 위 작업을 반복함
+ * 정렬이 돼있을 수록 느려짐 최악의 경우 O(n^2)
+'''
 import random
 
 def quick_sort( data, start, end ):
@@ -30,6 +37,26 @@ def quick_sort( data, start, end ):
     return
 
 
+def qsort( data ):
+    if len(data) <= 1:
+        return data
+
+    left, right = list(), list()
+    pivot = data[0]
+#     for index in range( 1, len(data) ):
+#         if pivot > data[index]:
+#             left.append(data[index])
+#         else :
+#             right.append(data[index])
+    left = [item for item in data[1:] if pivot > item ]
+    right = [item for item in data[1:] if pivot <= item ]            
+    return qsort(left) + [pivot] +qsort(right)
+
+
 data = random.sample( range(100), 10 )     
 quick_sort( data, 0, len( data )-1 )
 print( data )
+
+print( qsort(data) )
+
+
